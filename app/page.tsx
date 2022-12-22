3336dc15d34a8b56df747b499499ed11878ef0dc
Bobby Zhong <107908524+bobbyzhong@users.noreply.github.com>
2022-12-22 10:26:50 -0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+// download headwind extension to autosort classnames so we see the same thing
+export default function Home() {
+  return (
+    <div className="">
+      <div></div>
+      <button className="px-3 py-2 m-3 font-sans text-white bg-blue-500 rounded-2xl font-inter">
+        Click Me
+      </button>
+    </div>
+  )
+}
