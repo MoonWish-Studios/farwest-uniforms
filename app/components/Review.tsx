1cae36fa3cbcdc3188245bbb0e33ce8bb666f7bd
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-24 15:08:10 -0800

    added mobile responsiveness and lower min width for sm screen

--- app/components/Review.tsx
+++ app/components/Review.tsx
@@ -9,14 +9,14 @@ export interface ReviewProps {
 
 export default function Review({ src, name, message }: ReviewProps) {
   return (
-    <div className="relative w-72 h-72 border border-solid rounded-md bg-darkPurple border-lightPurple">
+    <div className="hover:bg-lightPurple relative sm:w-72 sm:h-72 border border-solid rounded-md bg-darkPurple border-lightPurple w-auto h-56">
       <img
         src={src}
         className="border-lightPurple border-solid border-4  absolute w-20 h-20 rounded-full top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       />
 
       <GradientText className="mt-12">{name}</GradientText>
-      <p className="text-textPrimary text-center px-4 text-base pb-6">{message}</p>
+      <p className="text-textPrimary text-center sm:px-4 px-12 py-4 text-base pb-6">{message}</p>
     </div>
   )
 }
--- app/components/ReviewSection.tsx
+++ app/components/ReviewSection.tsx
@@ -6,7 +6,7 @@ import Review, { ReviewProps } from "./Review"
 
 export default function ReviewSection() {
   return (
-    <div className="flex flex-wrap gap-x-6 gap-y-16 align-center">
+    <div className="flex transition-all flex-wrap gap-x-2 gap-y-16 justify-center align-center">
       {reviews.map(({ src, name, message, id }: ReviewProps) => (
         <Review src={src} name={name} message={message} />
       ))}
@@ -16,55 +16,41 @@ export default function ReviewSection() {
 
 const reviews = [
   {
-    src: "buddha.jpg",
+    src: "assets/buddha.jpg",
     name: "Emily I.",
     message:
       "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
     id: nanoid(),
   },
   {
-    src: "buddha.jpg",
+    src: "assets/buddha.jpg",
     name: "Emily I.",
     message:
       "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
     id: nanoid(),
   },
   {
-    src: "buddha.jpg",
+    src: "assets/buddha.jpg",
     name: "Emily I.",
     message:
       "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
     id: nanoid(),
   },
   {
-    src: "buddha.jpg",
+    src: "assets/buddha.jpg",
+    name: "Emily I.",
+    message:
+      "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
+  },
+  {
+    src: "assets/buddha.jpg",
     name: "Emily I.",
     message:
       "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
     id: nanoid(),
   },
   {
-    src: "buddha.jpg",
-    name: "Emily I.",
-    message:
-      "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
-    id: nanoid(),
-  },
-  {
-    src: "buddha.jpg",
-    name: "Emily I.",
-    message:
-      "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
-  },
-  {
-    src: "buddha.jpg",
-    name: "Emily I.",
-    message:
-      "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
-    id: nanoid(),
-  },
-  {
-    src: "buddha.jpg",
+    src: "assets/buddha.jpg",
     name: "Emily I.",
     message:
       "Amazing service. Needed a large patch sewn on my kids' Jujitsu uniform. They did a wonderful job and the price was so low I wish I had more uniforms to sew. Definitely recommend.",
